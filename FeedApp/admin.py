321af12cbbda1ffffd9c1a78dd4b768e6dc555e5
campbell <campbell.com>
2023-10-15 20:13:16 -0500

    project

--- FeedApp/admin.py
+++ FeedApp/admin.py
@@ -3,3 +3,8 @@ from django.contrib import admin
 # Register your models here.
 
 
+admin.site.register(Profile)
+admin.site.register(Relationship)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Like)
